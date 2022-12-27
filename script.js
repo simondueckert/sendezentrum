ba8dc9f9cd31ade100f348e1065e1c7084af93f1
Sascha <shiller@gmx.de>
2022-12-28 00:46:18 +0100

    Script test v0.2

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 WA.chat.sendChatMessage('Willkommen im Sendezentrum', 'Sendezentrum');
+
+const menu = WA.ui.registerMenuCommand('menu test',
+    {
+        callback: () => {
+            WA.chat.sendChatMessage('test');
+        }
+    })
+
+// Some time later, if you want to remove the menu:
+menu.remove();
