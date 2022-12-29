33756aac0190bde274a472ec3f8000fbd21445ae
Sascha <shiller@gmx.de>
2022-12-30 00:05:59 +0100

    Optische änderungen für Nacht

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // WA.chat.sendChatMessage('Willkommen im Sendezentrum', 'Sendezentrum');
 
-const myLayerSubscriber =  WA.room.area.onEnter("Lichtschalter").subscribe(() => {
-    //WA.room.showLayer("nacht");
-    WA.state.saveVariable('varnacht', true).catch(e => console.error('Something went wrong while saving variable', e));
-    console.log('Variable "varnacht" set. New value: true');
-  });
+// const myLayerSubscriber =  WA.room.area.onEnter("Lichtschalter").subscribe(() => {
+//     //WA.room.showLayer("nacht");
+//     WA.state.saveVariable('varnacht', true).catch(e => console.error('Something went wrong while saving variable', e));
+//     console.log('Variable "varnacht" set. New value: true');
+//   });
   
-  WA.room.area.onLeave("Lichtschalter").subscribe(() => {
-    WA.state.saveVariable('varnacht', false).catch(e => console.error('Something went wrong while saving variable', e));
-   // myLayerSubscriber.unsubscribe();
-  });
+//   WA.room.area.onLeave("Lichtschalter").subscribe(() => {
+//     WA.state.saveVariable('varnacht', false).catch(e => console.error('Something went wrong while saving variable', e));
+//    // myLayerSubscriber.unsubscribe();
+//   });
 
 
-// WA.state.onVariableChange('varnacht').subscribe((value) => {
-//     console.log('Variable "varnacht" changed. New value: ', value);
-//     if (value) {WA.room.showLayer("nacht");} else {WA.room.hideLayer("nacht");}
-// });
+WA.state.onVariableChange('varnacht').subscribe((value) => {
+    console.log('Variable "varnacht" changed. New value: ', value);
+    if (value) {WA.room.showLayer("nacht");} else {WA.room.hideLayer("nacht");}
+});
 
-// WA.onInit().then(() => {
-//     let value = WA.state.varnacht;
-//     if (value) {WA.room.showLayer("nacht");} else {WA.room.hideLayer("nacht");}
-//     console.log('Current player name: ', WA.player.name);
-// });
+WA.onInit().then(() => {
+    let value = WA.state.varnacht;
+    if (value) {WA.room.showLayer("nacht");} else {WA.room.hideLayer("nacht");}
+    console.log('Current player name: ', WA.player.name);
+});
---
+++ wc.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="../2021/world.css">
+        <link rel="stylesheet" href="../2021/sendezentrum.css">
+        <script src="https://play.workadventu.re/iframe_api.js"></script>
+    </head>
+    <body>
+
+        <!--<a href="javascript:WA.nav.closeCoWebSite();">Close me</a>-->
+        <h2>RC3-2021</h2>
+        <ul>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-436-podcasting-ler/oembed');">Podcasting lernen mit dem lernOS Podcasting Leitfaden - Simon Dückert</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-128-zur-lage-der-fiktion/oembed');">Zur Lage der Fiktion - Philipp Müllender</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-450-clubofnerds/oembed');">ClubOfNerds -  Martin Hillenbrand, Leon Haupt and Justin Scholz</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-222-entbehrliches/oembed');">Entbehrliches - Unterhaltsames Wissen aus der Wikipedia -  Flo (Poschi), Florian Baumann and derphilipp</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-173-ceelights-eint/oembed');">Ceelights Eintopf - Ceelight</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-180-gala-be-need-i/oembed');">Gala Be Need Inn - Die Podcast Quiz Show - MacSnider</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-449-ressourcenfm/oembed');">Ressourcen.FM - Martin Hillenbrand and Justin Scholz</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-479-audiodump/oembed');">Audiodump - Malik Aziz</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-2021-sendezentrum-439-philipps-podca/oembed');">Philipps Podcast Pastete - derphilipp</a></li>
+        </ul>
+        <h2>RC3-2020</h2>
+        <ul>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-496038-privacyweek_der_podcast/oembed');">PrivacyWeek - der Podcast</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-666278-realitaetstheorie_ein_podcast_ueber_scifi_und_die_wirklichkeit/oembed');">Realitätstheorie - Ein Podcast über SciFi und die Wirklichkeit</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-520410-funkwhale_and_the_importance_of_decentralized_podcasting/oembed');">Funkwhale and the Importance of Decentralized Podcasting </a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-324716-entbehrliches_unterhaltsames_wissen_aus_der_wikipedia/oembed');">Entbehrliches - Unterhaltsames Wissen aus der Wikipedia</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-673641-nsu_watch/oembed');">NSU-Watch: Aufklären & Einmischen - Der NSU-Watch Jahresrückblick 2020</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-404041-lernos_on_air_spacial_chat_with_workadventure/oembed');">lernOS on Air - Spacial Chat with Workadventure </a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-158-autistische-wahrnehmung/oembed');">Autistische Wahrnehmung </a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-793716-podcasting_als_produktive_unterrichtsmethode/oembed');">Podcasting als produktive Unterrichtsmethode</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-364640-ceelights_eintopf/oembed');">Katalonien Eintopf - Ein Crossover</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-681850-binaergewitter_talk_live/oembed');">Binärgewitter Talk Live</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-700718-ultraschall_5_eine_einfuehrung/oembed');">Ultraschall 5 - eine Einführung</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-158412-ogunhe_podcast_black_scientist/oembed');">Ogunhe Podcast - Black Scientist</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-396971-audiodump_podcast_live/oembed');">Audiodump Podcast live</a></li>
+            <li><a href="javascript:startVideo('https://media.ccc.de/v/rc3-166-sendezentrum-rc3-retrospektive/oembed');">Sendezentrum @ rC3 Retrospektive</a></li>
+        </ul>
+                
+<script>
+    async function startVideo(videoUrl)
+     {
+        const website = await WA.room.website.get('leinwand');
+        website.url = videoUrl;
+     }
+</script>
+</body>
+</html>
